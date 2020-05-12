42f6ef0331393d48b76d18a61ee01704d590df11
Amanda <neelyab@vcu.edu>
2020-05-12 10:01:47 -0400

    setup cors

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
+const { NODE_ENV, CLIENT_ORIGIN } = require('./config')
 
 const app = express()
 
@@ -13,10 +13,13 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors())
+app.use(cors({
+    origin: CLIENT_ORIGIN
+    })
+)
 
 app.get('/', (req, res) => {
-     res.send('Hello, Boilerplate!')
+     res.status(200).send('Hello boilerplate!')
      })
 
 app.use(function errorHandler(error, req, res, next) {
--- test/app.spec.js
+++ test/app.spec.js
@@ -5,6 +5,6 @@ describe('App', () => {
   it('GET / responds with 200 containing "Hello, boilerplate!"', () => {
     return supertest(app)
       .get('/')
-      .expect(200, 'Hello, Boilerplate!')
+      .expect(200, 'Hello boilerplate!')
   })
 })
