d5348f54ada7a3874062cf0f10e2732e63a5efb4
Amanda <neelyab@vcu.edu>
2020-05-20 19:43:12 -0400

    made cors global

--- src/app.js
+++ src/app.js
@@ -17,9 +17,7 @@ const morganOption = (NODE_ENV === 'production')
   : 'common';
 
 app.use(morgan(morganOption))
-app.use(cors({
-    origin: CLIENT_ORIGIN
-}));
+app.use(cors());
 app.use(helmet())
 
 app.use('/api/users', usersRouter)
