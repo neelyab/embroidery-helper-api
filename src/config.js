996754913c810bb7f111313d72cc803dc4b3c565
Amanda <neelyab@vcu.edu>
2020-05-20 11:52:50 -0400

    changed DATABASE_URL  for heroku

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    CLIENT_ORIGIN: 'http://localhost:3000',
-    DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin:paper@localhost/embroidery_helper',
+    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin:paper@localhost/embroidery_helper',
     JWT_SECRET: process.env.JWT_SECRET,
     JWT_EXPIRY: process.env.JWT_EXPIRY
   }
