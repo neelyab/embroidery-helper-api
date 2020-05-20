50a5efb36a853e1c5fa13d7bb045906a41dd2262
Amanda <neelyab@vcu.edu>
2020-05-20 18:37:19 -0400

    added client origin

--- test/jwtauth.spec.js
+++ test/jwtauth.spec.js
@@ -8,7 +8,7 @@ describe('Authentication Endpoint', () => {
     before('make connection', () => {
         db = knex({
             client: 'pg',
-            connection: process.env.TEST_DB_URL,
+            connection: process.env.TEST_DATABASE_URL,
         })
         app.set('db', db)
     })
--- test/saved-stitches.spec.js
+++ test/saved-stitches.spec.js
@@ -9,7 +9,7 @@ describe('GET Saved Stitches endpoints', () => {
     before('make knex instance', () => {
       db = knex({
         client: 'pg',
-        connection: process.env.TEST_DB_URL
+        connection: process.env.TEST_DATABASE_URL
       })
       app.set('db', db)
     })
