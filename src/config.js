3e37eba595c88d45477509d299100c2701e4e131
Amanda <neelyab@vcu.edu>
2020-05-20 18:42:13 -0400

    added client origin to config file

--- src/config.js
+++ src/config.js
@@ -3,5 +3,7 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin:paper@localhost/embroidery_helper',
     JWT_SECRET: process.env.JWT_SECRET,
-    JWT_EXPIRY: process.env.JWT_EXPIRY
+    JWT_EXPIRY: process.env.JWT_EXPIRY,
+    CLIENT_ORIGIN: 'https://embroidery-helper-e1ucqqsi2.now.sh'
+}
   }
